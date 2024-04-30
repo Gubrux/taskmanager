03a39a47ac44028aab5237bb1c5585d3f7055cad
Gubrux <gabrielcentur@gmail.com>
2024-04-30 19:02:58 -0400

    crea el projectRouter

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { ProjectController } from "../controllers/ProjectController";
+import { body, param } from 'express-validator';
+import { handleInputErrors } from "../middleware/validation";
+
+const router = Router();
+
+
+router.post('/',
+    body('projectName')
+
+    .notEmpty().withMessage('Project name is required'),
+    body('clientName')
+    .notEmpty().withMessage('Project client is required'),
+    body('description')
+    .notEmpty().withMessage('Project description is required'),
+    handleInputErrors,
+    ProjectController.createProject);
+
+    router.get("/", ProjectController.getAllProjects);
+
+router.get("/:id",
+    param('id').isMongoId().withMessage('Invalid project id'),
+    handleInputErrors,
+    ProjectController.getAllProjectsById);
+    
+router.put("/:id",
+    param('id').isMongoId().withMessage('Invalid project id'),
+    body('projectName')
+    .notEmpty().withMessage('Project name is required'),
+    body('clientName')
+    .notEmpty().withMessage('Project client is required'),
+    body('description')
+    .notEmpty().withMessage('Project description is required'),
+    handleInputErrors,
+    ProjectController.updateProject);
+
+
+
+
+export default router;
