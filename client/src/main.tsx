6863295e8d4c4b25b6fca3bb65600fa32ebf7690
Gubrux <gabrielcentur@gmail.com>
2024-05-04 19:54:55 -0400

    add the router to the main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Router from "./router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <h1>holo</h1>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Router />
+    </React.StrictMode>
+);
