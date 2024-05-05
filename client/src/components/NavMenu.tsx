ea04a2f097293d12c1afc961d5fe32b13beae768
Gubrux <gabrielcentur@gmail.com>
2024-05-04 22:46:37 -0400

    deleted a commentary

--- client/src/components/NavMenu.tsx
+++ client/src/components/NavMenu.tsx
@@ -21,7 +21,6 @@ export default function NavMenu() {
             >
                 <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
                     <div className="w-full lg:w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
-                        {/* saludo */}
                         <p className="text-center">Hola: Usuario</p>
                         <Link
                             to="/profile"
