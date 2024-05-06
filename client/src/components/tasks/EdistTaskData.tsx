d96ce00f7eb9457ac50e053330a82527bd04c3ab
Gubrux <gabrielcentur@gmail.com>
2024-05-06 00:11:27 -0400

    add the edit taks data and modal

---
+++ client/src/components/tasks/EdistTaskData.tsx
@@ -0,0 +1,20 @@
+import { getTaskById } from "@/api/TaskAPI";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation, useParams } from "react-router-dom";
+import EditTaskModal from "./EditTaskModal";
+
+export default function EdistTaskData() {
+    const params = useParams();
+    const projectId = params.projectId!;
+
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const taskId = queryParams.get("editTask")!;
+
+    const { data } = useQuery({
+        queryKey: ["editTask", taskId],
+        queryFn: () => getTaskById({ projectId, taskId }),
+        enabled: !!taskId,
+    });
+    if (data) return <EditTaskModal data={data} />;
+}
