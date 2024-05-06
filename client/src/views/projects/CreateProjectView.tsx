8df92b1b97c48aaea31e5687dbd42594d8dd54fd
Gubrux <gabrielcentur@gmail.com>
2024-05-06 00:16:04 -0400

    add some styles and fixed the dataflow

--- client/src/views/projects/CreateProjectView.tsx
+++ client/src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 import ProjectForm from "@/components/projects/ProjectForm";
-import { ProjectFormData } from "types";
+import { useMutation } from "@tanstack/react-query";
+import { ProjectFormData } from "@/types/index";
 import { createProject } from "@/api/ProjectAPI.ts";
 
 export default function CreateProjectView() {
+    const navigate = useNavigate();
     const initialValues: ProjectFormData = {
         projectName: "",
         clientName: "",
@@ -15,22 +18,32 @@ export default function CreateProjectView() {
         handleSubmit,
         formState: { errors },
     } = useForm({ defaultValues: initialValues });
-    const handleForm = (data: ProjectFormData) => {
-        createProject(data);
+    const { mutate } = useMutation({
+        mutationFn: createProject,
+        onError: (error: Error) => {
+            toast.error(error.message);
+        },
+        onSuccess: (data) => {
+            toast.success(data);
+            navigate("/");
+        },
+    });
+    const handleForm = (formData: ProjectFormData) => {
+        mutate(formData);
     };
     return (
         <>
             <div className="mx-auto max-w-3xl">
                 <h1 className="text-5xl font-black">Crear Proyecto</h1>
-                <p className="text-2xl font-light text-gray-500">
+                <p className="text-2xl font-light pt-4 text-gray-500">
                     Completa el formulario para crear un nuevo proyecto
                 </p>
                 <nav className="my-5">
                     <Link
                         to={"/"}
-                        className="bg-red-500 hover:bg-red-600 px-10 py-3 text-white text-xl font-bold cursor-pointer"
+                        className="bg-red-500 hover:bg-gradient-to-r from-red-500 via-red-400 to-red-500 px-10 py-3 text-white text-xl font-bold cursor-pointer"
                     >
-                        Volver atrás
+                        Volver
                     </Link>
                 </nav>
                 <form
@@ -43,7 +56,7 @@ export default function CreateProjectView() {
                     <input
                         type="submit"
                         value="Crear Proyecto"
-                        className="bg-red-600 hover:bg-red-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                        className="bg-sky-500 hover:bg-gradient-to-r from-sky-500 via-cyan-400 to-teal-500 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
                     />
                 </form>
             </div>
