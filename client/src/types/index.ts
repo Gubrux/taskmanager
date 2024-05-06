93b70c7150fd2868494ff07409c3be140f4a8e1d
Gubrux <gabrielcentur@gmail.com>
2024-05-06 00:13:08 -0400

    add the new task and dashboard schemas

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,13 +1,38 @@
 import { z } from "zod";
 
-export const projectsSchema = z.object({
+export const taskStatusSchema = z.enum([
+    "pending",
+    "onHold",
+    "inProgress",
+    "underReview",
+    "completed",
+]);
+export const taskSchema = z.object({
+    _id: z.string(),
+    name: z.string(),
+    description: z.string(),
+    project: z.string(),
+    status: taskStatusSchema,
+});
+export type Task = z.infer<typeof taskSchema>;
+export type TaskFormData = Pick<Task, "name" | "description">;
+
+export const projectSchema = z.object({
     _id: z.string(),
     projectName: z.string(),
     clientName: z.string(),
     description: z.string(),
 });
+export const dashboardProjectSchema = z.array(
+    projectSchema.pick({
+        _id: true,
+        projectName: true,
+        clientName: true,
+        description: true,
+    })
+);
 
-export type Project = z.infer<typeof projectsSchema>;
+export type Project = z.infer<typeof projectSchema>;
 export type ProjectFormData = Pick<
     Project,
     "projectName" | "clientName" | "description"
