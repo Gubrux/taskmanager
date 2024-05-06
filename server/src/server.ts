fdf6da49062074476a36fc66aeb6f1ca4eeba2fa
Gubrux <gabrielcentur@gmail.com>
2024-05-06 00:18:03 -0400

    add morgan and cors config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db';
-import projectRoutes from './routes/projectRoutes';
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import { corsConfig } from "./config/cors";
+import { connectDB } from "./config/db";
+import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
 connectDB();
 const app = express();
+// cors
+app.use(cors(corsConfig));
 
+app.use(morgan("dev"));
 
 app.use(express.json());
 
 // Routes
-app.use('/api/projects', projectRoutes)
-
+app.use("/api/projects", projectRoutes);
 
 export default app;
