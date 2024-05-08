2047509824db7a353621ceee425bea924ce0a851
Gubrux <gabrielcentur@gmail.com>
2024-05-08 11:52:22 -0400

    add routes for forgotpass updatepass & updatepass

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/authController";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
@@ -17,7 +17,7 @@ router.post(
         }
         return true;
     }),
-    body("email").isEmail().withMessage("email es invalido"),
+    body("email").isEmail().withMessage("email no es valido"),
     handleInputErrors,
     AuthController.createAccount
 );
@@ -30,16 +30,43 @@ router.post(
 );
 router.post(
     "/login",
-    body("email").notEmpty().withMessage("El email no valido"),
+    body("email").notEmpty().withMessage("El email no es valido"),
     body("password").notEmpty().withMessage("La contraseña es invalida"),
     handleInputErrors,
     AuthController.login
 );
 router.post(
     "/request-code",
-    body("email").notEmpty().withMessage("El email no valido"),
+    body("email").notEmpty().withMessage("El email no es valido"),
     handleInputErrors,
     AuthController.requestConfirmationCode
+);
+router.post(
+    "/forgot-password",
+    body("email").notEmpty().withMessage("El email no es valido"),
+    handleInputErrors,
+    AuthController.forgotPassword
+);
+router.post(
+    "/validate-token",
+    body("token").notEmpty().withMessage("El token es requerido"),
+    handleInputErrors,
+    AuthController.validateToken
+);
+router.post(
+    "/update-password/:token",
+    param("token").isNumeric().withMessage("El token es invalido"),
+    body("password")
+        .isLength({ min: 8 })
+        .withMessage("la contraseña debe tener al menos 8 caracteres"),
+    body("password_confirmation").custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error("Las contraseñas no coinciden");
+        }
+        return true;
+    }),
+    handleInputErrors,
+    AuthController.updatePassWithToken
 );
 
 export default router;
