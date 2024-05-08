6fa063715db3aa25524b10150c6859d5f3bb8550
Gubrux <gabrielcentur@gmail.com>
2024-05-08 11:53:06 -0400

    implemented jwt in backend

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -4,6 +4,7 @@ import { checkPassword, hashPassword } from "../utils/auth";
 import Token from "../models/Token";
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../emails/AuthEmail";
+import { generateJWT } from "../utils/jwt";
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
@@ -90,7 +91,8 @@ export class AuthController {
                 const error = new Error("Contraseña incorrecta");
                 return res.status(401).json({ error: error.message });
             }
-            res.send("Usuario logueado!!");
+            const token = generateJWT({id: user._id});
+            res.send(token);
         } catch (error) {
             console.error(error);
             res.status(500).send("Internal Server Error");
@@ -123,6 +125,63 @@ export class AuthController {
             // Save the user and the token
             await Promise.allSettled([token.save(), user.save()]);
             res.send("se envio un nuevo token");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        }
+    };
+    static forgotPassword = async (req: Request, res: Response) => {
+        try {
+            const { email } = req.body;
+            const user = await User.findOne({ email });
+            if (!user) {
+                const error = new Error("El usuario no esta registrado");
+                return res.status(409).json({ error: error.message });
+            }
+            //generar el token
+            const token = new Token();
+            token.token = generateToken();
+            token.user = user.id;
+            await token.save();
+            // Send confirmation email
+            AuthEmail.sendPasswordResetToken({
+                email: user.email,
+                name: user.name,
+                token: token.token,
+            });
+            res.send("Revisa tu correo para reestablecer tu contraseña");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        }
+    };
+    static validateToken = async (req: Request, res: Response) => {
+        try {
+            const { token } = req.body;
+            const tokenExists = await Token.findOne({ token }).populate("user");
+            if (!tokenExists) {
+                const error = new Error("Token invalido");
+                return res.status(404).json({ error: error.message });
+            }
+            res.send("Token valido, puedes reestablecer tu contraseña");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        }
+    };
+    static updatePassWithToken = async (req: Request, res: Response) => {
+        try {
+            const { token } = req.params;
+            const { password } = req.body;
+            const tokenExists = await Token.findOne({ token });
+            if (!tokenExists) {
+                const error = new Error("Token invalido");
+                return res.status(404).json({ error: error.message });
+            }
+            const user = await User.findById(tokenExists.user);
+            user.password = await hashPassword(password);
+            await Promise.allSettled([user.save(), tokenExists.deleteOne()]);
+            res.send("La contraseña ha sido actualizada!!");
         } catch (error) {
             console.error(error);
             res.status(500).send("Internal Server Error");
