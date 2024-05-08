b83a2f071a77eddd315e76e13eeba8f8c2c2aed4
Gubrux <gabrielcentur@gmail.com>
2024-05-08 11:49:10 -0400

    created the forgotpassview

---
+++ client/src/views/auth/ForgotPasswordView.tsx
@@ -0,0 +1,96 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { ForgotPasswordForm } from "@/types/index";
+import ErrorMessage from "@/components/ErrorMessage";
+import { useMutation } from "@tanstack/react-query";
+import { forgotPassword } from "@/api/AuthAPI";
+import { toast } from "react-toastify";
+export default function ForgotPasswordView() {
+    const initialValues: ForgotPasswordForm = {
+        email: "",
+    };
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({ defaultValues: initialValues });
+
+    const { mutate } = useMutation({
+        mutationFn: forgotPassword,
+        onError: (error: Error) => {
+            toast.error(error.message);
+        },
+        onSuccess: () => {
+            toast.success("Instrucciones enviadas");
+            reset();
+        },
+    });
+
+    const handleForgotPassword = (formData: ForgotPasswordForm) =>
+        mutate(formData);
+
+    return (
+        <>
+            <h1 className="text-3xl font-black text-white">
+                Olvidaste tu contraseña?
+            </h1>
+            <p className="text-xl font-light text-white">
+                Coloca tu email para {""}
+                <span className=" text-sky-500 font-bold">
+                    {" "}
+                    reestablecer tu contraseña
+                </span>
+            </p>
+            <form
+                onSubmit={handleSubmit(handleForgotPassword)}
+                className="space-y-8 p-10 mt-5  bg-white"
+                noValidate
+            >
+                <div className="flex flex-col gap-5">
+                    <label className="font-normal text-2xl" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Email de Registro"
+                        className="w-full p-3  border-gray-300 border"
+                        {...register("email", {
+                            required: "El Email de registro es obligatorio",
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "E-mail no válido",
+                            },
+                        })}
+                    />
+                    {errors.email && (
+                        <ErrorMessage>{errors.email.message}</ErrorMessage>
+                    )}
+                </div>
+
+                <input
+                    type="submit"
+                    value="Enviar Instrucciones"
+                    className="bg-sky-500 hover:bg-gradient-to-r from-sky-500 via-cyan-400 to-teal-500 w-full p-3  text-white font-black  text-xl cursor-pointer"
+                />
+            </form>
+
+            <nav className="mt-10 flex flex-col space-y-4">
+                <Link
+                    to="/auth/login"
+                    className="text-center text-gray-300 font-normal"
+                >
+                    ¿Ya tienes cuenta? Iniciar Sesión
+                </Link>
+
+                <Link
+                    to="/auth/register"
+                    className="text-center text-gray-300 font-normal"
+                >
+                    ¿No tienes cuenta? Crea una
+                </Link>
+            </nav>
+        </>
+    );
+}
