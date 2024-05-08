c103aa0b1d0612c36a76594695f282ce7a08e944
Gubrux <gabrielcentur@gmail.com>
2024-05-08 11:46:57 -0400

    add the forgotpass and update pass methods

--- client/src/api/AuthAPI.ts
+++ client/src/api/AuthAPI.ts
@@ -2,6 +2,8 @@ import api from "@/lib/axios";
 import { isAxiosError } from "axios";
 import {
     ConfirmToken,
+    ForgotPasswordForm,
+    NewPasswordForm,
     RequestConfirmationCodeForm,
     UserLoginForm,
     UserRegistrationForm,
@@ -49,6 +51,45 @@ export async function requestConfirmationCode(
 export async function authenticateUser(formData: UserLoginForm) {
     try {
         const url = "/auth/login";
+        const { data } = await api.post(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+export async function forgotPassword(formData: ForgotPasswordForm) {
+    try {
+        const url = "/auth/forgot-password";
+        const { data } = await api.post(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+export async function validateToken(formData: ConfirmToken) {
+    try {
+        const url = "/auth/validate-token";
+        const { data } = await api.post(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+export async function updatePasswordWithToken({
+    formData,
+    token,
+}: {
+    formData: NewPasswordForm;
+    token: ConfirmToken["token"];
+}) {
+    try {
+        const url = `/auth/update-password/${token}`;
         const { data } = await api.post(url, formData);
         return data;
     } catch (error) {
