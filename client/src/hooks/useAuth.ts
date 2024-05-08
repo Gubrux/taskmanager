cb8a0f456eda95c655a3c206a99ff3a26ced3e58
Gubrux <gabrielcentur@gmail.com>
2024-05-08 17:18:40 -0400

    add the useAuth hook

---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "@/api/AuthAPI";
+
+export function useAuth() {
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["user"],
+        queryFn: getUser,
+        retry: 1,
+        refetchOnWindowFocus: false,
+    });
+    return { data, isLoading, isError };
+}
