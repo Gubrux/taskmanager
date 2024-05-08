f7759171b2ce3ee0d63174d24c70f9d573d7635d
Gubrux <gabrielcentur@gmail.com>
2024-05-08 11:47:54 -0400

    add forgot pass and new pass routes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardView from "@/views/DashboardView";
 import AppLayout from "@/layouts/AppLayout";
-import "./index.css";
-import CreateProjectView from "@/views/projects/CreateProjectView";
+import AuthLayout from "@/layouts/AuthLayout";
+import LoginView from "@/views/auth/LoginView";
+import DashboardView from "@/views/DashboardView";
+import RegisterView from "@/views/auth/RegisterView";
 import EditProjectView from "@/views/projects/EditProjectView";
+import CreateProjectView from "@/views/projects/CreateProjectView";
 import ProjectDetailsView from "@/views/projects/ProjectDetailsView";
-import AuthLayout from "./layouts/AuthLayout";
-import LoginView from "./views/auth/LoginView";
-import RegisterView from "./views/auth/RegisterView";
-import ConfirmAccountView from "./views/auth/ConfirmAccountView";
-import RequestNewCodeView from "./views/auth/RequestNewCodeView";
+import ConfirmAccountView from "@/views/auth/ConfirmAccountView";
+import RequestNewCodeView from "@/views/auth/RequestNewCodeView";
+import ForgotPasswordView from "@/views/auth/ForgotPasswordView";
+import NewPasswordView from "./views/auth/NewPasswordView";
+
+import "./index.css";
 
 export default function Router() {
     return (
@@ -40,6 +43,14 @@ export default function Router() {
                     <Route
                         path="/auth/request-code"
                         element={<RequestNewCodeView />}
+                    />
+                    <Route
+                        path="/auth/forgot-password"
+                        element={<ForgotPasswordView />}
+                    />
+                    <Route
+                        path="/auth/new-password"
+                        element={<NewPasswordView />}
                     />
                 </Route>
             </Routes>
