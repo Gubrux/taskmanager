6a46a4300448bde3f57c3bf7f80d595b30539d6f
Gubrux <gabrielcentur@gmail.com>
2024-05-16 15:04:59 -0400

    feat: Add Spaceman component for 404 page

---
+++ client/src/views/404/Spaceman.tsx
@@ -0,0 +1,10 @@
+export default function Spaceman() {
+    return (
+        <>
+            <img
+                className="aspect-video"
+                alt="404 not found"
+            />
+        </>
+    );
+}
